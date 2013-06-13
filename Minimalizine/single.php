2452630ead8d6f5afbf88a91992374b0e37877ac
RYO NAGAI <seinaoasis@gmail.com>
2013-06-13 14:18:45 +0900

    投稿日表示方法の変更

--- Minimalizine/single.php
+++ Minimalizine/single.php
@@ -4,7 +4,7 @@
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div class="mcontent">    
     	<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-        <div class="submain"><?php the_time('F, d Y') ?>  </div>
+        <div class="submain"><?php the_time('Y年 Md日') ?>  </div>
     	<div class="clear"></div>
         <div class="maincontent">
             <?php the_content(); ?>
